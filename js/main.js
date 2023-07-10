3b0c87570369d9a7bfa09df16d1a18c40f0706cf
NikolGraminova <nbackup619@gmail.com>
2023-07-10 09:58:54 +0200

    Added price calculator and discount codes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
+let discountCodes = ["test", "YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
 function submitForm(event) {
     event.preventDefault();
     console.log("test");
+    let userHours = document.getElementById("inputHours").value;
+    let typeWork = document.getElementById("inputWork").value;
+    let userDiscount = document.getElementById("inputCode").value;
+
+    let price;
+    if (typeWork == "backend") {
+        price = userHours * 20.5;
+    } else if (typeWork == "frontend") {
+        price = userHours * 15.3;
+    } else if (typeWork == "analysis") {
+        price = userHours * 33.6;
+    }
+
+    if (discountCodes.includes(userDiscount)) {
+        price = price / 100 * 75;
+    }
+
+    console.log(price);
 }
